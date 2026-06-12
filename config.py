0903c23efe680d22ca1da43fa0674112009751d8
uqodirov404-ai <uqodirov404@gmail.com>
2026-06-13 04:05:06 +0500

    Add config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+﻿import os
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8776480971:AAG72ZrvGsf4zcGDHQlsB3zl2k2hLbivqgc")
+DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://neondb_owner:npg_zeIA3WkV5uEM@ep-jolly-glade-atlpo5xx-pooler.c-9.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require")
+GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY", "AIzaSyCX6V24rjEsM56Q-yRH98lFTeUJShItSx4")
+WEBAPP_URL = os.environ.get("WEBAPP_URL", "https://esse-expert-ai-bot.onrender.com")
